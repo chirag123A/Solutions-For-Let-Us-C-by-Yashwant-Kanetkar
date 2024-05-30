b9a701ebe73745ed751bb507fb9ed12269b1e79a
Chirag Ahuja <ahujachirag42@gmail.com>
2024-05-30 11:16:19 +0530

    oK

--- ch1q3.c
+++ ch1q3.c
@@ -4,9 +4,9 @@ int main()
     int i, w=841, h=1189, t;
      for(i=0;i<9;i++)
      {
-         printf("\n A%d: %d x %d", i, w, h);
-         t = h;
-         h = w;
+        printf("\n A%d: %d x %d", i, w, h);
+        t = h;
+        h = w;
         w = t/2;
      }
      return (0);
